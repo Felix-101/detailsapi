b82b0c75ca60ca3548ca1262209e5d7ed108f19e
Felix-101 <kupoluyitolulope452@gmail.com>
2024-07-01 19:02:49 +0100

    second commit

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const express= require('express')
-const User_details= require('./Routes/user_details')
-const connectDb = require('./util')
-const app= express()
+const express = require('express');
+const User_detailsRouter = require('./Routes/user_details'); // Renamed for clarity
+const connectDb = require('./util');
+const app = express();
 
-app.use(express.json())
-app.use(User_details)
-connectDb()
-app.listen(3000, ()=>{
-    console.log('Sever is running on port 3000')
-})
-; // Adjust the path as necessary
+app.use(express.json());
+// Correctly use the router for all routes starting with '/api'
+app.use('/api', User_detailsRouter);
 
-
+connectDb();
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
